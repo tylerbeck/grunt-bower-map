492da2d17bf066342f88faed1186479e3451d987
Tyler Beck <tyler@tylerbeck.com>
2015-02-25 23:25:38 -0500

    added grunt-bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,6 +95,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-bump');
 	grunt.loadNpmTasks('grunt-bower-install-simple');
 
 	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
